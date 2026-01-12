0797c5494a67bfb9fa8ac24062d6d76d12b2d196
Nireeksha <nirupoojary684@gmail.com>
2026-01-12 17:44:33 +0530

    updated

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Vehicle, Cart, SellExchange, LoanRequest
 
-# Register your models here.
+admin.site.register(Vehicle)
+admin.site.register(Cart)
+admin.site.register(SellExchange)
+admin.site.register(LoanRequest)
