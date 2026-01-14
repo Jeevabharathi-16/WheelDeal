e13ad5dbb652b2d0e7a3df510f2784791eae8ec1
Nireeksha <nirupoojary684@gmail.com>
2026-01-14 17:22:15 +0530

    Fixed loan error and UI updates

--- cars/views.py
+++ cars/views.py
@@ -126,14 +126,21 @@ def signup(request):
 
 def user_login(request):
     if request.method == 'POST':
-        user = authenticate(
-            username=request.POST['username'],
-            password=request.POST['password']
-        )
-        if user:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('home')  
+        else:
+            return render(request, 'login.html', {
+                'error': 'Invalid username or password'
+            })
+
     return render(request, 'login.html')
+
 
 
 def user_logout(request):
